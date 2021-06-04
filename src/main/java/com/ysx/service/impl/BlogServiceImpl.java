f57bfe3fb836c763909bd7535a3292b2b9360c5c
ysx <691765069@qq.com>
2021-06-04 23:51:30 +0800

    add archives page

--- src/main/java/com/ysx/service/impl/BlogServiceImpl.java
+++ src/main/java/com/ysx/service/impl/BlogServiceImpl.java
@@ -19,9 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ysx.util.MyBeanUtils;
 
 import javax.persistence.criteria.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class BlogServiceImpl implements BlogService {
@@ -100,6 +98,21 @@ public class BlogServiceImpl implements BlogService {
         Sort sort = Sort.by(Sort.Direction.DESC,"updateTime");
         Pageable pageable = PageRequest.of(0,size,sort);
         return blogRepository.findTop(pageable);
+    }
+
+    @Override
+    public Map<String, List<Blog>> archiveBlog() {
+        List<String> years= blogRepository.findGroupYear();
+        Map<String,List<Blog>> map = new HashMap<>();
+        for(String year: years){
+            map.put(year,blogRepository.findByYear(year));
+        }
+        return map;
+    }
+
+    @Override
+    public Long countBlog() {
+        return blogRepository.count();
     }
 
     @Transactional
